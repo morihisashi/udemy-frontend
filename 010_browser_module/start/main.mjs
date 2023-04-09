5f5c1ba914a304e292ee2228c339f92d815da93d
morihisashi <hisa.1995.322@outlook.com>
2023-04-09 21:51:14 +0900

    ESMとcommonjsの違いについて

--- 010_browser_module/start/main.mjs
+++ 010_browser_module/start/main.mjs
@@ -1,4 +1,6 @@
 import { helloFromSub } from "./sub.mjs";
-
+import isOdd from './node_modules/is-odd/index.js';
 // const myModule = require('./sub.js');
 helloFromSub();
+const oddy = isOdd(3);
+console.log(oddy);
